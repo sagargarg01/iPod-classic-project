c1914d0929fc73e1c82e492bf45089c2ba3a48da
Sagar Garg <sagargupta1417@gmail.com>
2020-05-13 17:21:37 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import menu from './assets/menu.svg'
+import fastforward from './assets/fast_forward.svg'
+import play_pause from './assets/play_pause.svg'
+import rewind from './assets/rewind.svg'
 
 function App() {
   return (
@@ -15,8 +18,12 @@ function App() {
         <div className="buttons-container">
           <div className="buttons"> 
           <div className="inner-disk">
-            {/* <img src = {menu}/> */}
+
           </div>
+          <img src = {menu} className="menu"/>
+            <img src = {play_pause} className="play_pause"/>
+            <img src = {fastforward} className="fastforward"/>
+            <img src = {rewind} className="rewind"/>
           </div>
         </div>
 
