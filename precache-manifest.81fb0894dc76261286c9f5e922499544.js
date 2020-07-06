feca901ff1b4d79b5442632735334d9d7dc91748
Sagar Garg <sagargupta1417@gmail.com>
2020-07-06 14:46:22 +0530

    Updates

---
+++ precache-manifest.81fb0894dc76261286c9f5e922499544.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde7322a3bbb0cb3f08d78e09131278e",
+    "url": "/iPod-classic-project/index.html"
+  },
+  {
+    "revision": "a2271ec59242c1e172cd",
+    "url": "/iPod-classic-project/static/css/main.e64f09bc.chunk.css"
+  },
+  {
+    "revision": "f41aec143d449d6d3db4",
+    "url": "/iPod-classic-project/static/js/2.f4f9eabf.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/iPod-classic-project/static/js/2.f4f9eabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2271ec59242c1e172cd",
+    "url": "/iPod-classic-project/static/js/main.20d82ddd.chunk.js"
+  },
+  {
+    "revision": "4f47eb26b6d6518ce1f3",
+    "url": "/iPod-classic-project/static/js/runtime-main.aabfc982.js"
+  },
+  {
+    "revision": "dfac59c228743e115cef310ded9f809d",
+    "url": "/iPod-classic-project/static/media/Closer.dfac59c2.mp3"
+  },
+  {
+    "revision": "827c00fad22985110e1243061c0915ea",
+    "url": "/iPod-classic-project/static/media/arrow_right.827c00fa.svg"
+  },
+  {
+    "revision": "f1c09248363110f907cee7375c9ced81",
+    "url": "/iPod-classic-project/static/media/battery.f1c09248.svg"
+  },
+  {
+    "revision": "20b461b32eef4852e4d7fcfd4be5e241",
+    "url": "/iPod-classic-project/static/media/closer.20b461b3.png"
+  },
+  {
+    "revision": "98a1be9bdd7fa0dfe09462058250f0e9",
+    "url": "/iPod-classic-project/static/media/closer.98a1be9b.jpg"
+  },
+  {
+    "revision": "45ca51505a4f6e3297a5b034955d9384",
+    "url": "/iPod-classic-project/static/media/dice.45ca5150.svg"
+  },
+  {
+    "revision": "71f81b396f1fe9f3f6e3f6fdde202144",
+    "url": "/iPod-classic-project/static/media/fast_forward.71f81b39.svg"
+  },
+  {
+    "revision": "ce51f257a5c7359302e531cbf4a3a871",
+    "url": "/iPod-classic-project/static/media/games-icon.ce51f257.png"
+  },
+  {
+    "revision": "e032a7b43315c33e2b92a00bb4be3a01",
+    "url": "/iPod-classic-project/static/media/ipod_logo.e032a7b4.svg"
+  },
+  {
+    "revision": "8485f50039edb6c512eadf5ef0044f1e",
+    "url": "/iPod-classic-project/static/media/menu.8485f500.svg"
+  },
+  {
+    "revision": "f3eeb9be61569f44d847e04728c57041",
+    "url": "/iPod-classic-project/static/media/pause.f3eeb9be.svg"
+  },
+  {
+    "revision": "9f3edea8117f4a87952bc937d677fdf6",
+    "url": "/iPod-classic-project/static/media/play.9f3edea8.svg"
+  },
+  {
+    "revision": "222e916295ac267568b5df5c780bb244",
+    "url": "/iPod-classic-project/static/media/play_pause.222e9162.svg"
+  },
+  {
+    "revision": "fec587ae218a4e07db7140670a07c78d",
+    "url": "/iPod-classic-project/static/media/rewind.fec587ae.svg"
+  }
+]);
